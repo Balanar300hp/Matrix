d7f1f70351c6147698f92e005986f369956ca978
Balanar300hp <kudashov.vlad@yandex.ru>
2016-03-14 21:29:35 +0300

    Update header.h

--- header.h
+++ header.h
@@ -15,16 +15,12 @@ public:
 	friend Matrix operator+(const Matrix &firstMatrix, const Matrix &secondMatrix);
 	friend Matrix operator*(const Matrix &firstMatrix, const Matrix &secondMatrix);
 	int* operator [](int i);
-	void   GetSumRows(const Matrix &firstMatrix, const Matrix &secondMatrix, int i);
-	void   GetCompositionRows(const Matrix &firstMatrix, const Matrix &secondMatrix, int i);
 	void   NewMemory();
 	int get_rows();
 	int get_columns();
-	int Get(int rows, int columns) const;
-	void Set(int row, int columns, int value);
 private:
-	string s;
-	int rows;
-	int columns;
-	int **_matrix;
+	string s{};
+	int rows{};
+	int columns{};
+	int **_matrix{};
 };
